0dd4ec462832e2f003977e80159c5bf3d2b80387
surbhi7849 <surbhi7849@gmail.com>
2023-12-05 23:52:08 +0100

    committing changes and new files to the repository

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+import uvicorn
+from io import BytesIO
+from your_model_module import find_similar_images, display_images # Import your ML model and functions here
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    input_image = Image.open(BytesIO(contents)).convert("RGB")
+
+    # Save the uploaded image temporarily if needed
+    input_image_path = "temp_input_image.jpg"
+    input_image.save(input_image_path)
+
+    # Assuming you have a function to find similar images
+    similar_images = find_similar_images(input_image_path, image_dataset_paths, model, processor)
+
+    # Generate and return an HTML page with the input and similar images displayed
+    # You would need to modify display_images function to return an HTML string or handle it here
+    html_content = generate_html_content(input_image_path, similar_images)
+    return HTMLResponse(content=html_content, status_code=200)
+
+def generate_html_content(input_image_path, similar_images):
+    # Implement a function that generates an HTML string to display images
+    # This could use <img> tags to show the input and similar images
+    pass
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Visual Echoes: Airbnb Edition API"}
+
+# Additional routes and logic as needed
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
